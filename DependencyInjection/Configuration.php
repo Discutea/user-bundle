87db2d81eaf47809820efa683f8cdcf7f4c4299a
David <contact@discutea.com>
2020-01-21 10:40:30 +0100

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -7,6 +7,8 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
+    const ALIAS = 'md_socom';
+
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder('discutea_user');
